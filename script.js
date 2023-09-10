96ced620a88209c3fb58ff6217d0924ab98c1400
Raiz Farhan <raiezarshd@gmail.com>
2023-09-10 14:59:05 +0800

    Update

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
-const url="https://pokeapi.co/api/v2/pokemon/";
-const card = document.getElementById("card");
-const btn = document.getElementById("btn");
-
-const typeColor =
-{
+const typeColor = {
     bug: "#26de81",
     dragon: "#ffeaa7",
     electric: "#fed330",
@@ -20,87 +15,80 @@ const typeColor =
     psychic: "#a29bfe",
     rock: "#2d3436",
     water: "#0190FF",
-    steel: "#dfe6e9",
-    dark: "#2d3436"
-}
-
-let getPokeData = () =>
-{
-    let id = Math.floor(Math.random() * 999) + 1;
-
+  };
+  const url = " https://pokeapi.co/api/v2/pokemon/";
+  const card = document.getElementById("card");
+  const btn = document.getElementById("btn");
+  
+  let getPokeData = () => {
+    // Generate a random number between 1 and 150
+    let id = Math.floor(Math.random() * 150) + 1;
+    // Combine the pokeapi url with pokemon id
     const finalUrl = url + id;
-    fetch(finalUrl).then((response)=>response.json()).
-    then((data)=>{
+    // Fetch generated URL
+    fetch(finalUrl)
+      .then((response) => response.json())
+      .then((data) => {
         generateCard(data);
-    })
-};
-
-
-let generateCard = (data) =>
-{
+      });
+  };
+  
+  //Generate Card
+  
+  let generateCard = (data) => {
+    // Get necessary data and assign it to variables
     console.log(data);
     const hp = data.stats[0].base_stat;
-    const imgSrc = data.sprites.front_default;
-    const name = data.name[0].toUpperCase() + data.name.slice(1);
-    const attack = data.stats[1].base_stat; 
-    const defense = data.stats[2].base_stat;
-    const speed = data.stats[5].base_stat;
-
+    const imgSrc = data.sprites.other.dream_world.front_default;
+    const pokeName = data.name[0].toUpperCase() + data.name.slice(1);
+    const statAttack = data.stats[1].base_stat;
+    const statDefense = data.stats[2].base_stat;
+    const statSpeed = data.stats[5].base_stat;
+  
+    // Set themeColor based on pokemon type
     const themeColor = typeColor[data.types[0].type.name];
-
+    console.log(themeColor);
     card.innerHTML = `
-    <p class="hp">
-    <span>HP</span>
-    ${hp}
-    </p>
-    <img src=${imgSrc} alt="demo">
-    <h2 class="poke-name">${name}</h2>
-    <div class="types">
-
-    </div>
-    <div class="stats">
-        <div id = "attack">
-        <h3>Attack</h3>
-        <p>${attack}</p>
-       
-        </div>
-
-        <div id = "defense">
-            <h3>Defense</h3>
-            <p>${defense}</p>
-        </div>
-
-        <div id = "speed">
-            <h3>Speed</h3>
-            <p>${speed}</p>
-        </div>
-    </div>
+          <p class="hp">
+            <span>HP</span>
+              ${hp}
+          </p>
+          <img src=${imgSrc} />
+          <h2 class="poke-name">${pokeName}</h2>
+          <div class="types">
+           
+          </div>
+          <div class="stats">
+            <div>
+              <h3>${statAttack}</h3>
+              <p>Attack</p>
+            </div>
+            <div>
+              <h3>${statDefense}</h3>
+              <p>Defense</p>
+            </div>
+            <div>
+              <h3>${statSpeed}</h3>
+              <p>Speed</p>
+            </div>
+          </div>
     `;
-
     appendTypes(data.types);
     styleCard(themeColor);
-};
-
-let appendTypes = (types) =>
-{
-    let typesContainer = document.querySelector(".types");
-    types.forEach((type)=>{
-        let typeSpan = document.createElement("span");
-        typeSpan.innerText = type.type.name;
-        typesContainer.appendChild(typeSpan);
-    })
-};
-
-let styleCard = (themeColor) =>
-{
-    card.style.background = `radial-gradient(circle at 50% 0%, ${themeColor} 36%, #ffffff 36%)`;
-    card.querySelectorAll(".types span").forEach((type) =>
-    {
-
-        type.style.background = typeColor[type.innerText];
-
+  };
+  let appendTypes = (types) => {
+    types.forEach((item) => {
+      let span = document.createElement("SPAN");
+      span.textContent = item.type.name;
+      document.querySelector(".types").appendChild(span);
     });
-};
-
-btn.addEventListener("click", getPokeData);
-window.addEventListener("load", getPokeData);
+  };
+  let styleCard = (color) => {
+    card.style.background = `radial-gradient(circle at 50% 0%, ${color} 36%, #ffffff 36%)`;
+    card.querySelectorAll(".types span").forEach((typeColor) => {
+      typeColor.style.backgroundColor = color;
+    });
+  };
+  
+  btn.addEventListener("click", getPokeData);
+  window.addEventListener("load", getPokeData);
